e071f1497b1431ea23c05377ef9b3f091c897d59
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-09-11 21:47:53 -0300

    add

--- app/src/main/java/br/com/etecia/boston/LoginActivity.java
+++ app/src/main/java/br/com/etecia/boston/LoginActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.boston;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,16 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+
+        });
+
     }
+
 }
--- app/src/main/java/br/com/etecia/boston/SplashActivity.java
+++ app/src/main/java/br/com/etecia/boston/SplashActivity.java
@@ -25,7 +25,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(new Intent(getApplicationContext(), SplashActivity.class));
             }
         },2000);
     }
