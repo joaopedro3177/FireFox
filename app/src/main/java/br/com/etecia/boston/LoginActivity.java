5fdb42af88edb52217fbb694ee663f5f2e864a09
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-09-11 22:13:38 -0300

    add3

--- app/src/main/java/br/com/etecia/boston/LoginActivity.java
+++ app/src/main/java/br/com/etecia/boston/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
     Button btnEntrar;
+    TextView txtCadstro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
         btnEntrar = findViewById(R.id.btnEntrar);
+        txtCadstro = findViewById(R.id.txtCadastro);
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +35,13 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), MenuPrincipalActivity.class));
             }
 
+        });
+
+        txtCadstro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RecuperarSenhaActivity.class));
+            }
         });
 
     }
